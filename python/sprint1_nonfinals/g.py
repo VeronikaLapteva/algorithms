95da24957de21b328aaf11f5489613a98c341450
unknown <stas.basov@whatever.com>
2023-06-14 10:23:01 +0300

    solve nonfinal g

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,19 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    m = ''
+    while number > 1:
+        if number % 2 == 1:
+            i = 1
+            m = str(i) + m
+        else:
+            i = 0
+            m = str(i) + m
+        number = number // 2
+    m = str(number) + m
+    return m
+
 
 def read_input() -> int:
     return int(input().strip())
+
 
 print(to_binary(read_input()))
